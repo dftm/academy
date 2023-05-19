e8ea42a83fef34695f615012d9af744e00c8a756
Dimitri E <dftm.jobs@gmail.com>
2023-05-19 10:15:07 +0300

    Update src.js

--- src.js
+++ src.js
@@ -1,8 +1,11 @@
-//CSS
-$('head').append('<link rel="stylesheet" type="text/css"/ href="https://cdn.jsdelivr.net/gh/dftm/academy@cf297b6/src.css">');
-
 //Nunito font
 $('head').append('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"/>');
+
+//Def CSS
+$('head').append('<link rel="stylesheet" type="text/css"/ href="https://cdn.jsdelivr.net/gh/dftm/academy@0c4c468/def.css">');
+
+//Custom CSS
+$('head').append('<link rel="stylesheet" type="text/css"/ href="https://cdn.jsdelivr.net/gh/dftm/academy@4e89ec3/src.css">');
 
 //check for brand image then show academy
 if ( $( ".navbar > .navbar-inner > .nav-container > a.brand>img" ).length ) {
