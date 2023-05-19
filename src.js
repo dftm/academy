7b291a6e70f57108b750fd9d6edb0e32f64e462d
Dimitri E <dftm.jobs@gmail.com>
2023-05-19 20:06:29 +0300

    Update src.js

--- src.js
+++ src.js
@@ -1,8 +1,11 @@
-//check for brand image then show academy
+// ――――― check for brand image then show academy ――――― //
 //if ( $( ".navbar > .navbar-inner > .nav-container > a.brand>img" ).length ) {
 //  $( ".navbar > .navbar-inner > .nav-container > a.brand" ).append( "<p id='Academy-nav-div'> Academy &#127963; </p>" );
 //}
 
-//logo-swithc
+// ――――― logo-switcheroo ――――― //
 $(".navbar > .navbar-inner > .nav-container > a.brand > img").attr("src", "https://cdn.jsdelivr.net/gh/dftm/academy@2860e6d/logo.png");
 //$(".navbar > .navbar-inner > .nav-container > a.brand > img").attr("width", "80%");
+
+// ――――― Code Highlights ――――― //
+hljs.highlightAll();
